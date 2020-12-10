1b4557f57d761993d6879871c54da342026e7a20
michael <mpthornton86@gmail.com>
2020-12-09 17:33:41 -0700

    Add source column to gemstones table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_07_204749) do
+ActiveRecord::Schema.define(version: 2020_12_09_195956) do
 
   create_table "gemstones", force: :cascade do |t|
     t.string "name"
     t.integer "user_id"
     t.string "properties"
+    t.string "source"
   end
 
   create_table "users", force: :cascade do |t|
