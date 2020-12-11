31e5022b05e11d9e57ea9a27e351e54bc63a40b9
michael <mpthornton86@gmail.com>
2020-12-10 18:28:42 -0700

    Add route protection to gemstone controller

--- app/controllers/gemstone_controller.rb
+++ app/controllers/gemstone_controller.rb
@@ -1,45 +1,65 @@
 class GemstoneController < ApplicationController
-    get '/lsit_all' do 
-        @gems = Gemstone.all
-        @user = User.all
-        erb :"gemstone/index"
-    end
 
-    get '/gemstone/new' do
-        if valid_user? == true
+    get '/new_gem' do
+        logged_in
         erb :"gemstone/new"
-        else
-            flash[:uh_oh] = "You must be logged in."
-            redirect '/login'
-        end
     end
 
-    post '/gemstone/new' do
+    post '/new_gem' do
         #binding.pry
-        gem = Gemstone.new(params[:gem])
-        gem.save
+        redirect_hacker
+        Gemstone.create(params[:gem])
         redirect "/user/#{params[:gem][:user_id]}/collection"
     end
 
     get '/gemstone/:id/edit' do
-        if valid_user? == true
-            @gem = Gemstone.find_by_id(params[:id])
+        #binding.pry
+        logged_in
+            @gem = Gemstone.find(params[:id])
+            not_authorized
         erb :"gemstone/edit"
-        else flash[:uh_oh] = "You must be logged in."
-            redirect '/login'
-        end
     end
 
     patch '/gemstone/:id/edit' do
-        gem = Gemstone.find_by_id(params[:id])
-        gem.update(params[:gem])
+        @gem = Gemstone.find(params[:id])
+        not_authorized
+        @gem.update(params[:gem])
         redirect "user/#{current_user.id}/collection"
     end
 
     delete '/gemstone/:id/delete' do
-        gem = Gemstone.find_by_id(params[:id])
-        gem.destroy
+        @gem = Gemstone.find(params[:id])
+        not_authorized
+        @gem.destroy
         redirect "/user/#{current_user.id}/collection"
+    end
+
+    get '/users_collections' do
+        logged_in
+            @user = User.all
+            erb :"users_collections/users_index"
+    end
+
+    get '/user/:id/view_collection' do
+        logged_in
+        @user = User.find(params[:id])
+        @gems = Gemstone.where(user_id: params[:id])
+        erb :"users_collections/collection_index"
+    end
+
+    private
+    def not_authorized
+        if session[:user_id] != @gem.user_id
+            flash[:not_allowed] = "You dont have access to that page."
+            redirect "/user/#{session[:user_id]}/home"
+        end
+    end
+
+    def redirect_hacker
+        if session[:user_id] != params[:gem][:user_id].to_i
+        flash[:not_allowed] = "Cannot change others collections."
+        redirect "/user/#{session[:user_id]}/home"
+        end
     end
 
 end
