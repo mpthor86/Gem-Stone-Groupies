7c85d00770e3edbae5107244b0e4f154b963aaca
michael <mpthornton86@gmail.com>
2020-12-11 17:20:54 -0700

    New table to migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_09_195956) do
+ActiveRecord::Schema.define(version: 2020_12_11_213003) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
+    t.integer "user_id"
+    t.integer "collection_id"
+  end
 
   create_table "gemstones", force: :cascade do |t|
     t.string "name"
