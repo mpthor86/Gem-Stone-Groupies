24b614c10a358e4e53b36aff67ea173f79bf14b2
michael <mpthornton86@gmail.com>
2020-12-12 17:36:47 -0700

    add comments to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
     has_many :gemstones
+    has_many :comments
     has_secure_password
 
+    validates :username, presence: true
     validates :username, uniqueness: true
 end
