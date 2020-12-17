17927ed2adb7a096075258b81580ebd04061ddd0
michael <mpthornton86@gmail.com>
2020-12-17 15:51:03 -0700

    add Gemstone validation

--- app/models/gemstone.rb
+++ app/models/gemstone.rb
@@ -1,3 +1,8 @@
 class Gemstone < ActiveRecord::Base
     belongs_to :user
+
+    validates :name, :properties, :source, presence: true
+    validates :name, length: 1..15
+    validates :properties, length: 1..35
+    validates :source, length: 1..15
 end
